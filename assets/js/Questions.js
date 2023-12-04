40d10e6685d238b6dadcaf1d46bc0dd3b2125909
AzzaAbdelrahman <148109380+AzzaAbdelrahman@users.noreply.github.com>
2023-12-04 21:36:44 +0000

    audio added

---
+++ assets/js/Questions.js
@@ -0,0 +1,59 @@
+
+const quizQuestions = [
+    {
+        question: "JavaScript is a language in",
+        Choices: ["Object- Oriented", "Objec-based", "procedural", "None of the above"],
+        correctAnswer: "Object-Oriented"
+    },
+
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        Choices: ["var", "let", "Both var and let", "None of the above"],
+        correctAnswer: "Both var and let"
+    },
+
+    {
+        question: "What is the most popular front-end web development library?",
+        choices: ["jQuery", "React", "Angular", "Vue"],
+        correctAnswer: "jQuery"
+    },
+
+    {
+        question: "How do you select an element with id 'demo'?",
+        choices: ["demo", "#demo", ".demo", "demo"],
+        correctAnswer: "#demo"
+    },
+
+    {
+        question: "What is the most popular front-end web development language for creating web applications?",
+        choices: ["JavaScript", "PHP", "Python", "Ruby"],
+        correctAnswer: "JavaScript"  
+    },
+
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        Choices: ["document.write()", "console.log()", "window.alert()", "All of of the above"],
+        correctAnswer: "All of of the above"
+    },
+
+
+    {
+        question: "What is the most popular front-end web development language for creating user interfaces?",
+        choices: ["HTML", "CSS", "JavaScript", "TypeScript"],
+        correctAnswer: "HTML"
+      },
+
+
+
+      {
+        question: "What is the most popular front-end web development framework?",
+        choices: ["Bootstrap", "React", "Angular", "Vue"],
+        correctAnswer: "React"
+      },
+
+
+      
+
+
+
+]
---
+++ assets/js/Scores.js
@@ -0,0 +1,11 @@
+// user score
+var userScore = 0;
+
+// sounds when a correct answer is chosen
+var correctAudio = document.createElement ("audio");
+correctAudio.setAttribute ('src', 'assets/sfx/correct.wav');
+
+
+// sounds when a incorrect answer is chosen
+var correctAudio = document.createElement ("audio");
+incorrectAnswer.setAttribute ('src', 'assets/sfx/incorrect.wav');
