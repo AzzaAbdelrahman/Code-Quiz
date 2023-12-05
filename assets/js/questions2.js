1297f11aeab09dbe7b77d25f6401b7c4b523ae73
AzzaAbdelrahman <148109380+AzzaAbdelrahman@users.noreply.github.com>
2023-12-05 21:43:40 +0000

    commit

---
+++ assets/js/questions2.js
@@ -0,0 +1,47 @@
+const quizQuestions = [
+    {
+        question: "What is the most popular front-end web development library?",
+        choices: ["jQuery", "React", "Angular", "Vue"],
+        correctAnswer: "jQuery"
+
+
+    },
+    {
+        question: "How do you select an element with id 'demo'?",
+        choices: ["demo", "#demo", ".demo", "demo"],
+        correctAnswer: "#demo"
+
+    },
+
+    {
+        question: "What is the most popular front-end web development language for creating web applications?",
+        choices: ["JavaScript", "PHP", "Python", "Ruby"],
+        correctAnswer: "JavaScript"
+
+
+
+
+
+    },
+
+    {
+        question: "What is the most popular front-end web development language for creating user interfaces?",
+        choices: ["HTML", "CSS", "JavaScript", "TypeScript"],
+        correctAnswer: "HTML"
+
+
+
+
+    },
+    {
+        question: "What is the most popular front-end web development framework?",
+        choices: ["Bootstrap", "React", "Angular", "Vue"],
+        correctAnswer: "React"
+
+
+
+    }
+
+   
+
+];
